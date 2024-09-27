0ea20361a0667458bdb7043ff3e887166a969c97
jhemj <ngmptdz@gmail.com>
2024-09-27 13:08:13 +0900

    commit

---
+++ delivery/src/main/java/labshopcompensation/domain/DeliveryStarted.java
@@ -0,0 +1,27 @@
+package labshopcompensation.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import labshopcompensation.domain.*;
+import labshopcompensation.infra.AbstractEvent;
+import lombok.*;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private String address;
+    private String customerId;
+    private Integer quantity;
+
+    public DeliveryStarted(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryStarted() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
